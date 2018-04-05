85c34af743a1d76dbe6c5e1d0b5b86b1fc962eea
Super Proyectos <rolandoandrade.estudio@gamil.com>
2018-04-05 11:04:14 -0400

    Comentarios

--- core/src/com/upsidedown/EscenaPrincipal.java
+++ core/src/com/upsidedown/EscenaPrincipal.java
@@ -8,81 +8,104 @@ import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.Event;
 import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.actions.Actions;
-import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
-import com.badlogic.gdx.scenes.scene2d.actions.RepeatAction;
-import com.badlogic.gdx.scenes.scene2d.actions.RotateByAction;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.upsidedown.punave.Nave;
 
-import static com.badlogic.gdx.scenes.scene2d.actions.Actions.delay;
-import static com.badlogic.gdx.scenes.scene2d.actions.Actions.forever;
-import static com.badlogic.gdx.scenes.scene2d.actions.Actions.rotateTo;
-import static com.badlogic.gdx.scenes.scene2d.actions.Actions.scaleTo;
-import static com.badlogic.gdx.scenes.scene2d.actions.Actions.sequence;
-
 public class EscenaPrincipal implements Screen
 {
-	private SpriteBatch batch;
-	private Tablero tablero;//Campo de juego.
-	private Stage stage;
+	//Variables de escena
 	private Game Juego;
+	private Stage stage;
+	//Variables de renderizado
+	private SpriteBatch batch;
 	private ShapeRenderer shape;
+	//Escenario
+	private Tablero tablero;//Campo de juego.
 	private ImageButton ponernave;
 	private ImageButton congelar;
-	private boolean hayNave;
+	//Fondo
+	private float a,r,g,b;
 	private Music musica;
+	//Power Ups
 	private Nave nave;
-	private float a;
-	private float r,g,b;
+
+	/*<----------CONSTRUCTOR---------->*/
+
 	public EscenaPrincipal(Game UnJuego)
 	{
-
+		//Inicializamos variables
 		Juego = UnJuego;
-
-		hayNave=false;
 		stage = new Stage(new ScreenViewport());
-		Config.escenario = stage;
-		Gdx.input.setInputProcessor(stage);
 		batch = new SpriteBatch();
-
-
-		Array<Texture> textures = new Array<Texture>();
-		for (int i = 1; i <= 4; i++)
-		{
-			textures.add(new Texture(Gdx.files.internal("Parallax" + i + ".png")));
-			textures.get(textures.size - 1).setWrap(Texture.TextureWrap.MirroredRepeat, Texture.TextureWrap.MirroredRepeat);
-		}
-		Fondo fondo = new Fondo(textures);
-		fondo.setSize(Config.w, Config.h / 2);
-		fondo.setSpeed(1);
-		stage.addActor(fondo);
-
-		a=0;
 		tablero = new Tablero();
-		stage.addActor(tablero);
 		shape = new ShapeRenderer();
+		crearFondo();
+		a=0;//Angulo-Hora del día
+
+		//Power ups
 		ponernave = new ImageButton(new TextureRegionDrawable(new TextureRegion(new Texture("nave.png"))));
 		congelar=new ImageButton(new TextureRegionDrawable(new TextureRegion(new Texture("nieve.png"))));
 		crearBoton(ponernave,0);
 		crearBoton(congelar,1);
-		stage.addActor(ponernave);
-		stage.addActor(congelar);
 		powerUps();
 
 		r=181;g=235;b=238;
+		generaEscenario();
 	}
+
+	/*<----------GENERAR ESCENARIO---------->
+	* Añade los objetos al escenario*/
+
+	private void generaEscenario()
+	{
+		Config.escenario = stage;
+		Gdx.input.setInputProcessor(stage);
+		//Añadimos al escenario
+		stage.addActor(tablero);
+		stage.addActor(ponernave);
+		stage.addActor(congelar);
+	}
+
+	/*<----------CREAR FONDO---------->
+	* Se crea un fondo, se configura y se agrega al escenario*/
+
+	private void crearFondo()
+	{
+		//Creamos el fondo
+		Fondo fondo = new Fondo(cargarImagenes());
+		//Configuramos el tamaño y la velocidad
+		fondo.setSize(Config.w, Config.h / 2);
+		fondo.setSpeed(1);
+		//Añadimos al escenario
+		stage.addActor(fondo);
+	}
+
+	/*<----------CARGAR IMÁGENES---------->
+	* Devuelve un arreglo con las texturas a utilizar de fondo*/
+
+	private Array<Texture>  cargarImagenes()
+	{
+		Array<Texture> texturas = new Array<Texture>();
+		for (int i = 1; i <= 4; i++)
+		{
+			texturas.add(new Texture(Gdx.files.internal("Parallax" + i + ".png")));
+			texturas.get(texturas.size - 1).setWrap(Texture.TextureWrap.MirroredRepeat, Texture.TextureWrap.MirroredRepeat);
+		}
+		return texturas;
+	}
+
+	/*<----------CREAR BOTON---------->
+	* Dado un botón establece su posición, dimensiones y eje de rotación*/
+
 	private void crearBoton(ImageButton boton,int i)
 	{
 		boton.setWidth(Config.w*2/13);
@@ -92,30 +115,21 @@ public class EscenaPrincipal implements Screen
 		boton.getImage().setOriginX(boton.getWidth()/2);
 	}
 
-	private void limpiar()
-	{
-		a=(float)((a+0.001)%(float)Math.PI);
-		float ax=Math.abs((float)Math.cos(a));
-		Gdx.gl.glClearColor(r*ax / 255f, g*ax / 255f, b*ax / 255f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		Gdx.gl.glEnable(GL20.GL_BLEND);
-		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-
-
-	}
+	/*<----------ACCIONES DE LOS POWERUPS---------->
+	* Establece los eventos al presionar sobre un PU*/
 
 	private void powerUps()
 	{
+		//Invocar una nave.
 		ponernave.addListener(new EventListener()
 		{
 			@Override
 			public boolean handle(Event event)
 			{
-				if(!hayNave)
+				if(nave==null)
 				{
 					nave=new Nave(Config.w/2,Config.h/5);
 					stage.addActor(nave);
-					hayNave=true;
 					Config.SONIDOS[1].play();
 					tablero.salida();
 				}
@@ -123,6 +137,7 @@ public class EscenaPrincipal implements Screen
 				return false;
 			}
 		});
+		//Congelar escenario.
 		congelar.addListener(new EventListener()
 		{
 			@Override
@@ -134,23 +149,55 @@ public class EscenaPrincipal implements Screen
 			}
 		});
 	}
+
+	/*<----------ANIMACIÓN DE ROTAR EN LOS POWERUPS---------->
+	* Rota los PU*/
+
 	private void rotar()
 	{
 		float ax=(float)Math.sin(a*100)*20;
 		ponernave.getImage().setRotation(ax);
 		congelar.getImage().setRotation(ax);
 	}
+
+	/*<----------ESTADOS DE LOS POWERUPS---------->
+	* Detecta si se acabó un PU*/
+
 	private void estados()
 	{
 		if(nave!=null&&nave.getDisparos()>=10)
 		{
 			nave.remove();
 			nave=null;
-			hayNave=false;
 			Config.SONIDOS[5].play();
 			tablero.entrada();
 		}
 	}
+
+	/*<----------CICLO DÍA NOCHE---------->
+	* Transcurso del día - noche*/
+
+	private void cicloDiario()
+	{
+		//Sumamos al ángulo y se obtiene el residuo para evitar overflow.
+		a=(float)((a+0.001)%(float)Math.PI);
+		//Calculamos el coseno. cos(0)=1 Día. cos(pi/2)=0 Noche.
+		float ax=Math.abs((float)Math.cos(a));
+		Gdx.gl.glClearColor(r*ax / 255f, g*ax / 255f, b*ax / 255f, 1);
+	}
+
+	/*<----------LIMPIAR ESCENARIO---------->
+	* Resetea el escenario*/
+
+	private void limpiar()
+	{
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glEnable(GL20.GL_BLEND);
+		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+	}
+
+	/*<----------MÉTODOS PREDETERMINADOS---------->*/
+
 	@Override
 	public void show()
 	{
@@ -162,6 +209,7 @@ public class EscenaPrincipal implements Screen
 	@Override
 	public void render(float delta)
 	{
+		cicloDiario();
 		limpiar();
 		rotar();
 		stage.act(Gdx.graphics.getDeltaTime());
